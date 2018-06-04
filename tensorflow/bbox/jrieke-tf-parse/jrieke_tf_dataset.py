7357964f5599cc4047a9f619d216f19d593de65e
Gustavo Valiati <gustavovaliati@gmail.com>
2018-06-04 19:13:12 -0300

    code upgraded to improve debugging

--- tensorflow/bbox/jrieke-tf-parse/jrieke_tf_dataset.py
+++ tensorflow/bbox/jrieke-tf-parse/jrieke_tf_dataset.py
@@ -13,19 +13,20 @@ class JriekeBboxDataset:
         # Create images with random rectangles and bounding boxes.
         num_imgs = 50000
 
-        self.img_size = 8
+        self.WIDTH = 8
+        self.HEIGHT = 8
         min_object_size = 1
         max_object_size = 4
         num_objects = 1
 
         self.bboxes = np.zeros((num_imgs, num_objects, 4))
-        self.imgs = np.zeros((num_imgs, self.img_size, self.img_size))  # set background to 0
+        self.imgs = np.zeros((num_imgs, self.WIDTH, self.HEIGHT))  # set background to 0
 
         for i_img in range(num_imgs):
             for i_object in range(num_objects):
                 w, h = np.random.randint(min_object_size, max_object_size, size=2)
-                x = np.random.randint(0, self.img_size - w)
-                y = np.random.randint(0, self.img_size - h)
+                x = np.random.randint(0, self.WIDTH - w)
+                y = np.random.randint(0, self.HEIGHT - h)
                 self.imgs[i_img, x:x+w, y:y+h] = 1.  # set rectangle to 1
                 self.bboxes[i_img, i_object] = [x, y, w, h]
 
@@ -37,7 +38,7 @@ class JriekeBboxDataset:
 
         # Normalize x, y, w, h by self.img_size, so that all values are between 0 and 1.
         # Important: Do not shift to negative values (e.g. by setting to mean 0), because the IOU calculation needs positive w and h.
-        y = self.bboxes.reshape(num_imgs, -1) / self.img_size
+        y = self.bboxes.reshape(num_imgs, -1) / self.WIDTH
         y.shape, np.mean(y), np.std(y)
 
         # Split training and test.
@@ -47,9 +48,28 @@ class JriekeBboxDataset:
         train_y = y[:i]
         test_y = y[i:]
         self.test_imgs = self.imgs[i:]
-        test_bboxes = self.bboxes[i:]
+        self.test_bboxes = self.bboxes[i:]
+
+        ### check if the generated imgs match to the test_X slice image
+        # self.check_dataset_image_compability(test_X[0], self.test_imgs[0])
 
         return train_X, train_y, test_X, test_y
+
+    def check_dataset_image_compability(self, test_X_sample, test_imgs_sample):
+        fig = plt.figure(figsize=(12, 3))
+        fig.suptitle('check if the generated imgs match to the test_X slice image')
+        fig.subplots_adjust(top=0.85)
+
+        plt.subplot(1, 2, 1)
+        plt.gca().set_title('Returned by the dataset class: used for training')
+        TMP = test_X_sample.reshape(self.WIDTH, self.HEIGHT)
+        plt.imshow(TMP.T, cmap='Greys', interpolation='none', origin='lower', extent=[0, self.WIDTH, 0, self.HEIGHT])
+
+        plt.subplot(1, 2, 2)
+        plt.gca().set_title('Global image holder: used for plotting.')
+        plt.imshow(test_imgs_sample.T, cmap='Greys', interpolation='none', origin='lower', extent=[0, self.WIDTH, 0, self.HEIGHT])
+        plt.show()
+        print('compare:',TMP,test_imgs_sample)
 
     def IOU(self,bbox1, bbox2):
         '''Calculate overlap between two bounding boxes [x, y, w, h] as the area of intersection over the area of unity'''
@@ -66,23 +86,92 @@ class JriekeBboxDataset:
 
         return I / U
 
+    def convertDefaultAnnotToCoord(self, annot):
+        '''
+        annot -> [x, y, w, h]
+        '''
+
+        w = annot[2] * self.WIDTH
+        h = annot[3] * self.HEIGHT
+
+        x = annot[0] * self.HEIGHT
+        y = annot[1] * self.HEIGHT
+
+        return [x,y,w,h]
+
+    def convertYoloAnnotToCoord(self, yolo_annot):
+        '''
+        yolo_annot -> [x, y, w, h]
+        '''
+
+        w = yolo_annot[2] * self.WIDTH
+        h = yolo_annot[3] * self.HEIGHT
+
+        x = (yolo_annot[0] * self.WIDTH) - (w/2)
+        y = (yolo_annot[1] * self.HEIGHT) - (h/2)
+
+        return [x,y,w,h]
+
     def show_generated(self, i=0):
-        plt.imshow(self.imgs[i].T, cmap='Greys', interpolation='none', origin='lower', extent=[0, self.img_size, 0, self.img_size])
+        fig = plt.figure()
+        fig.subplots_adjust(top=0.85)
+        fig.suptitle('Generated image sample + GT')
+        plt.imshow(self.imgs[i].T, cmap='Greys', interpolation='none', origin='lower', extent=[0, self.WIDTH, 0, self.HEIGHT])
         for bbox in self.bboxes[i]:
             plt.gca().add_patch(matplotlib.patches.Rectangle((bbox[0], bbox[1]), bbox[2], bbox[3], ec='r', fc='none'))
+            plt.gca().legend(['GT'])
         plt.show()
 
-    def show_predicted(self, pred_bboxes, test_bboxes):
-        # Show a few images and predicted bounding boxes from the test dataset.
-        print(pred_bboxes[0])
-        # print(pred_bboxes)
-        plt.figure(figsize=(12, 3))
-        for i_subplot in range(1, 5):
-            plt.subplot(1, 4, i_subplot)
-            i = np.random.randint(len(self.test_imgs))
-            plt.imshow(self.test_imgs[i].T, cmap='Greys', interpolation='none', origin='lower', extent=[0, self.img_size, 0, self.img_size])
-            for pred_bbox, exp_bbox in zip(pred_bboxes[i], test_bboxes[i]):
-                plt.gca().add_patch(matplotlib.patches.Rectangle((pred_bbox[0], pred_bbox[1]), pred_bbox[2], pred_bbox[3], ec='r', fc='none'))
-                # plt.annotate('IOU: {:.2f}'.format(self.IOU(pred_bbox, exp_bbox)), (pred_bbox[0], pred_bbox[1]+pred_bbox[3]+0.2), color='r')
+    def plot_rectangle(self, img, bbox):
+
+        fig = plt.figure()
+        fig.suptitle('Plotting rectangle.')
+        fig.subplots_adjust(top=0.85)
+
+        plt.subplot(1, 1, 1)
+        plt.imshow(img, cmap='Greys', interpolation='none', origin='lower', extent=[0, self.WIDTH, 0, self.HEIGHT])
+        plt.gca().add_patch(matplotlib.patches.Rectangle((bbox[0], bbox[1]), bbox[2], bbox[3], ec='r', fc='none'))
         plt.show()
-        plt.savefig('plots/bw-single-rectangle_prediction_{0:%Y-%m-%d%H:%M:%S}.png'.format(datetime.datetime.now()), dpi=300)
+
+
+    def check_dataset_image_compability(self, test_X_sample, test_imgs_sample):
+        fig = plt.figure(figsize=(12, 3))
+        fig.suptitle('check if the generated imgs match to the test_X slice image')
+        fig.subplots_adjust(top=0.85)
+
+        plt.subplot(1, 2, 1)
+        plt.gca().set_title('Returned by the dataset class: used for training')
+        plt.imshow(TMP.T, cmap='Greys', interpolation='none', origin='lower', extent=[0, self.WIDTH, 0, self.HEIGHT])
+
+        plt.subplot(1, 2, 2)
+        plt.gca().set_title('Global image holder: used for plotting.')
+        plt.imshow(test_imgs_sample.T, cmap='Greys', interpolation='none', origin='lower', extent=[0, self.WIDTH, 0, self.HEIGHT])
+        plt.show()
+        print('compare:',TMP,test_imgs_sample)
+
+    def show_predicted(self, pred_bboxes):
+        # Show a few images and predicted bounding boxes from the test dataset.
+
+        fig = plt.figure(figsize=(12, 3))
+        fig.subplots_adjust(top=0.85)
+        fig.suptitle('Prediction demonstration. Random samples.')
+        legend_plotted = False
+
+        for i_subplot in range(1, 11):
+            plt.subplot(1, 10, i_subplot)
+            i = np.random.randint(len(pred_bboxes))
+            plt.imshow(self.test_imgs[i].T, cmap='Greys', interpolation='none', origin='lower', extent=[0, self.WIDTH, 0, self.HEIGHT])
+            for pred_bbox, exp_bbox in zip(pred_bboxes[i], self.test_bboxes[i]):
+                # print('before convertion: pred',pred_bbox, 'gt',exp_bbox)
+                pred_bbox = self.convertDefaultAnnotToCoord(pred_bbox)
+                # exp_bbox = self.convertDefaultAnnotToCoord(exp_bbox)
+                print('after convertion: pred',pred_bbox, 'gt',exp_bbox)
+                plt.gca().add_patch(matplotlib.patches.Rectangle((pred_bbox[0], pred_bbox[1]), pred_bbox[2], pred_bbox[3], ec='r', fc='none'))
+                #gt
+                plt.gca().add_patch(matplotlib.patches.Rectangle((exp_bbox[0], exp_bbox[1]), exp_bbox[2], exp_bbox[3], ec='b', fc='none'))
+                plt.annotate('IOU: {:.2f}'.format(self.IOU(pred_bbox, exp_bbox)), (pred_bbox[0], pred_bbox[1]+pred_bbox[3]+0.2), color='r')
+                if not legend_plotted:
+                    legend_plotted = True
+                    plt.gca().legend(['Pred','GT'],loc='upper center', bbox_to_anchor=(0.5, -0.5), fancybox=True)
+        plt.show()
+        # plt.savefig('plots/bw-single-rectangle_prediction_{0:%Y-%m-%d%H:%M:%S}.png'.format(datetime.datetime.now()), dpi=300)
--- tensorflow/bbox/jrieke-tf-parse/tf-single-rectangle.py
+++ tensorflow/bbox/jrieke-tf-parse/tf-single-rectangle.py
@@ -4,12 +4,11 @@ from __future__ import print_function
 
 import numpy as np
 import tensorflow as tf
+import argparse
 
 from jrieke_tf_dataset import JriekeBboxDataset
 
 tf.logging.set_verbosity(tf.logging.INFO)
-
-CHECKPOINT_PATH='/tmp/convnet_model/model.ckpt-80004'
 
 def cnn_model_fn(features, labels, mode):
     # model = Sequential([
@@ -19,21 +18,22 @@ def cnn_model_fn(features, labels, mode):
     #         Dense(y.shape[-1])
     #     ])
     # model.compile('adadelta', 'mse')
-    input_layer = tf.reshape(features["x"], [-1, 8, 8, 1])
-    flat = tf.reshape(input_layer, [-1, 8 * 8 * 1])
+    # input_layer = tf.reshape(features["x"], [-1, 8, 8, 1])
+    flat = tf.reshape(features["x"], [-1, 8 * 8 * 1])
     dense = tf.layers.dense(inputs=flat, units=200, activation=tf.nn.relu)
     dropout = tf.layers.dropout(
                 inputs=dense,
                 rate=0.2,
                 training=mode == tf.estimator.ModeKeys.TRAIN)
     predicted_output = tf.layers.dense(inputs=dropout, units=4)#x y w h
+    print('predicted_output',predicted_output)
     predictions = {
         # Generate predictions (for PREDICT and EVAL mode)
         # "classes": tf.argmax(input=predicted_output, axis=1),
         "bboxes": predicted_output,
         # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
         # `logging_hook`.
-        "probabilities": tf.nn.softmax(predicted_output, name="softmax_tensor")
+        # "probabilities": tf.nn.softmax(predicted_output, name="softmax_tensor")
     }
 
     if mode == tf.estimator.ModeKeys.PREDICT:
@@ -61,33 +61,57 @@ def cnn_model_fn(features, labels, mode):
         mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
 def main(unused_argv):
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-c", "--checkpoint",
+                    required = False,
+                    default = False,
+                    help = "Load tensorflow checkpoint.")
+    args = vars(ap.parse_args())
+    CHECKPOINT_PATH = None
+    if args['checkpoint']:
+        CHECKPOINT_PATH = args['checkpoint']
+
     print("TensorFlow version: {}".format(tf.__version__))
     dataset = JriekeBboxDataset()
     train_data, train_labels, eval_data, eval_labels = dataset.generate()
+    print('len',len(train_data),len(train_labels), len(eval_data), len(eval_labels))
+    print(len(train_data), len(train_data[0]))
+    print(len(train_labels), len(train_labels[0]))
+    print(train_data[0],  train_labels[0])
+
     # dataset.show_generated()
 
     # Create the Estimator
     classifier = tf.estimator.Estimator(
-        model_fn=cnn_model_fn, model_dir="/tmp/convnet_model")
+        model_fn=cnn_model_fn, model_dir="./convnet_model")
 
     # Set up logging for predictions
     # Log the values in the "Softmax" tensor with label "probabilities"
-    tensors_to_log = {"probabilities": "softmax_tensor"}
-    logging_hook = tf.train.LoggingTensorHook(
-        tensors=tensors_to_log, every_n_iter=5000)
+    # tensors_to_log = {"probabilities": "softmax_tensor"}
+    # logging_hook = tf.train.LoggingTensorHook(
+        # tensors=tensors_to_log, every_n_iter=5000)
 
     # Train the model
     train_input_fn = tf.estimator.inputs.numpy_input_fn(
         x={"x": train_data},
         y=train_labels,
-        batch_size=100,
-        num_epochs=None,
+        # batch_size=100,
+        num_epochs=1,
         shuffle=True)
 
-    # classifier.train(
-    #     input_fn=train_input_fn,
-    #     steps=20000,
-    #     hooks=[logging_hook])
+    print('train_labels',train_labels[0])
+
+    # img = train_data[0].reshape(dataset.WIDTH, dataset.HEIGHT).T
+    # bbox = dataset.convertDefaultAnnotToCoord(train_labels[0])
+    # dataset.plot_rectangle(img, bbox)
+
+    if not CHECKPOINT_PATH:
+        print("Training...")
+        classifier.train(
+            input_fn=train_input_fn,
+            steps=None,
+            # hooks=[logging_hook]
+            )
 
     # Evaluate the model and print results
     eval_input_fn = tf.estimator.inputs.numpy_input_fn(
@@ -95,22 +119,41 @@ def main(unused_argv):
         y=eval_labels,
         num_epochs=1,
         shuffle=False)
-    eval_results = classifier.evaluate(
-        input_fn=eval_input_fn,
-        checkpoint_path=CHECKPOINT_PATH)
-    print(eval_results)
+
+    # img = eval_data[0].reshape(dataset.WIDTH, dataset.HEIGHT).T
+    # bbox = dataset.convertDefaultAnnotToCoord(eval_labels[0])
+    # dataset.plot_rectangle(img, bbox)
+
+    # eval_results = classifier.evaluate(
+    #     input_fn=eval_input_fn,
+    #     checkpoint_path=CHECKPOINT_PATH
+    #     )
+    # print(eval_results)
 
 
     predict_results = classifier.predict(
         input_fn=eval_input_fn,
-        checkpoint_path=CHECKPOINT_PATH)
+        checkpoint_path=CHECKPOINT_PATH
+        )
 
     # for prediction in predict_results:
         # print(prediction)
     # print(predict_results[0])
-
+    # print(list(predict_results)[0])
     pred_bboxes = [ [r['bboxes']] for r in predict_results]
-    dataset.show_predicted(pred_bboxes, eval_labels)
+    print('# of predicted images',len(pred_bboxes))
+    dataset.show_predicted(pred_bboxes)
+
+    '''
+    Keras:
+     - 6s - loss: 0.0248 - val_loss: 6.0943e-04
+    '''
+
+    summed_IOU = 0.
+    for pred_bbox, test_bbox in zip(predict_results, eval_labels):
+        summed_IOU += IOU(pred_bbox, test_bbox)
+    mean_IOU = summed_IOU / len(pred_bboxes)
+    print('mean_IOU:',mean_IOU)
 
 
 if __name__ == "__main__":
